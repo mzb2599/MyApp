bc9c1d216acc4cc8877496b43b8bfc71f15662b8
mzaki2599 <mzaki2599@gmail.com>
2025-05-18 12:07:05 +0530

    Todo list add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  [x: string]: any;
   title = 'MyApp';
+  newTask: string = '';
+  tasks: Task[] = [];
+  public completedTasks: Number = this.tasks.filter((task) => task.completed)
+    .length;
+
+  addTask() {
+    console.log(this.newTask);
+
+    if (this.newTask.trim()) {
+      this.tasks.push({
+        task: this.newTask,
+        completed: false,
+      });
+      this.newTask = '';
+    }
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
